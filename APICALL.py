eefef947dd64f106562c8a9c25e132eed8b01484
JADukeAIPI <109992842+JADukeAIPI@users.noreply.github.com>
2022-11-14 10:38:48 -0500

    Created the APICALL py file in order to import the API data to the DB. APICALL data also has been injected into the db. Also wrote some basic queries to verify the data was successfully loaded in the db.

---
+++ APICALL.py
@@ -0,0 +1,50 @@
+from bs4 import BeautifulSoup
+import pandas as pd
+import numpy as np
+import requests
+import matplotlib.pyplot as plt
+
+import requests
+
+def api_request():
+    url = "https://booking-com.p.rapidapi.com/v1/car-rental/search"
+
+    querystring = {"drop_off_longitude":"-84.419853","currency":"USD","sort_by":"recommended","drop_off_datetime":"2023-01-03 19:00:00","drop_off_latitude":"33.640411","from_country":"it","pick_up_longitude":"-84.419853","locale":"en-gb","pick_up_datetime":"2023-01-01 19:00:00","pick_up_latitude":"33.640411"}
+
+    headers = {
+	"X-RapidAPI-Key": "8bbb18c5b5mshcb2649ca8814f86p1d30a3jsn5a338ac42c22",
+	"X-RapidAPI-Host": "booking-com.p.rapidapi.com"
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    results = response.json()
+
+    df = pd.DataFrame.from_dict(results['search_results'])
+
+    df2 = pd.json_normalize(results, record_path =['search_results'])
+    return df2
+
+df2 = api_request()
+
+def clean_api_df(data_df):
+  df_cleaned = data_df.copy()
+  
+  #filter to the columns we want to keep
+  columns_to_keep = ['vehicle_info.v_id', 'vehicle_info.v_name', 'vehicle_info.group', 'vehicle_info.transmission', 'route_info.dropoff.city', 'pricing_info.base_price', 'route_info.pickup.city']
+  #filter to the types of vehicles in vehicle.group
+  vehicle_groups_to_keep = ['Luxury']
+  df_cleaned=df_cleaned[columns_to_keep]
+  df_cleaned=df_cleaned[df_cleaned['vehicle_info.group'].isin(vehicle_groups_to_keep)]
+  df_cleaned.rename(columns={'vehicle_info.v_id':'Vehicle_id', 'vehicle_info.v_name': 'Vehicle Name', 'vehicle_info.group': 'Category', 'pricing_info.base_price': 'Base Price','vehicle_info.transmission': 'Transmission', 'route_info.dropoff.city': 'Dropoff City', 'route_info.pickup.city': 'Pickup City'}, inplace=True)
+  df_cleaned.sort_values(by='Base Price', ascending=False, inplace=True)
+  return df_cleaned
+
+df_cleaned = clean_api_df(df2)
+
+
+#querystring2 = {"drop_off_longitude":"-84.419853","currency":"USD","sort_by":"recommended","drop_off_datetime":"2022-11-20 19:00:00","drop_off_latitude":"33.640411","from_country":"it","pick_up_longitude":"-84.419853","locale":"en-gb","pick_up_datetime":"2022-11-18 19:00:00","pick_up_latitude":"33.640411"}
+#response2 = requests.request("GET", url, headers=headers, params=querystring2)
+#results2 = response2.json()
+#df_attempt2 = pd.json_normalize(results2, record_path =['search_results'])
+#df_cleaned_attempt2 = clean_api_df(df_attempt2)
+#df_cleaned_attempt2
