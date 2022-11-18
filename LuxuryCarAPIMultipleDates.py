2987f84d9ab20d0be2f6854e3d090765b614e16f
mille055 <46400739+mille055@users.noreply.github.com>
2022-11-17 19:23:59 -0500

    Update LuxuryCarAPIMultipleDates.py
    Changed column names to eliminate spaces, calls for all cars and then can filter for luxury, tries to pickle the dataframes (but not sure if will work in GitHub)

--- LuxuryCarAPIMultipleDates.py
+++ LuxuryCarAPIMultipleDates.py
@@ -37,9 +37,9 @@ def clean_api_df(data_df):
   #filter to the types of vehicles in vehicle.group
   vehicle_groups_to_keep = ['Luxury']
   df_cleaned=df_cleaned[columns_to_keep]
-  df_cleaned=df_cleaned[df_cleaned['vehicle_info.group'].isin(vehicle_groups_to_keep)]
-  df_cleaned.rename(columns={'vehicle_info.v_id':'Vehicle_id', 'vehicle_info.v_name': 'Vehicle Name', 'vehicle_info.group': 'Category', 'pricing_info.base_price': 'Base Price', 'pricing_info.price': 'Price', 'vehicle_info.transmission': 'Transmission'}, inplace=True)
-  df_cleaned.sort_values(by='Base Price', ascending=False, inplace=True)
+  #df_cleaned=df_cleaned[df_cleaned['vehicle_info.group'].isin(vehicle_groups_to_keep)]
+  df_cleaned.rename(columns={'vehicle_info.v_id':'Vehicle_id', 'vehicle_info.v_name': 'Vehicle_Name', 'vehicle_info.group': 'Category', 'pricing_info.base_price': 'Base_Price', 'pricing_info.price': 'Price', 'vehicle_info.transmission': 'Transmission'}, inplace=True)
+  df_cleaned.sort_values(by='Base_Price', ascending=False, inplace=True)
   return df_cleaned
 
 def get_bookings_multiple_dates(start_date = get_date(1), end_date = get_date(30)):
@@ -53,8 +53,8 @@ def get_bookings_multiple_dates(start_date = get_date(1), end_date = get_date(30
     date_tuple = (day.strftime('%Y-%m-%d'), (day+timedelta(days=1)).strftime('%Y-%m-%d'))
 
     df_partial = clean_api_df(get_booking_data(*date_tuple))
-    df_partial['Date Pickup']=date_tuple[0]
-    df_partial['Date Dropoff'] = date_tuple[1]
+    df_partial['Date_Pickup']=date_tuple[0]
+    df_partial['Date_Dropoff'] = date_tuple[1]
     dfs.append(df_partial)
     day=day+timedelta(days=1)
 
@@ -64,21 +64,28 @@ def get_bookings_multiple_dates(start_date = get_date(1), end_date = get_date(30
   return df
 
 df_next_60_days = get_bookings_multiple_dates(get_date(1), get_date(60))
+df_next_60_days.to_pickle('Cars_next_60_days_df.pkl')
 
-cars_available = df_next_60_days.groupby('Date Pickup')['Vehicle Name'].count()
 
-cars_prices = df_next_60_days.groupby(['Vehicle_id', 'Date Pickup'])['Price'].mean()
+luxury_cars_next_60_days = df_next_60_days.copy()
+luxury_cars_next_60_days=luxury_cars_next_60_days[luxury_cars_next_60_days['vehicle_info.group']=='Luxury')]
+luxury_cars_next_60_days.to_pickle('Luxury_cars_next_60_days_df.pkl')
 
-price_each_day = df_next_60_days.groupby('Date Pickup')['Price'].mean()
+
+cars_available = df_next_60_days.groupby('Date_Pickup')['Vehicle_Name'].count()
+
+cars_prices = df_next_60_days.groupby(['Vehicle_id', 'Date_Pickup'])['Price'].mean()
+
+price_each_day = df_next_60_days.groupby('Date_Pickup')['Price'].mean()
 
 def create_df_for_model(df):
   df1=df.copy()
-  df1['cars_available'] = df1.groupby('Date Pickup')['Vehicle Name'].transform('count')
+  df1['cars_available'] = df1.groupby('Date_Pickup')['Vehicle_Name'].transform('count')
   fleet_size = df1['cars_available'].max()
   df1['Reservations'] = df1['cars_available'].apply(lambda x: fleet_size-x)
-  df1['Individual_car_prices'] = df1.groupby(['Vehicle_id', 'Date Pickup'])['Price'].transform('mean')
-  df1['Avg_type_car_prices'] = df1.groupby(['Vehicle Name', 'Date Pickup'])['Price'].transform('mean')
-  df1['Date Pickup'] = pd.to_datetime(df1['Date Pickup'])
+  df1['Individual_car_prices'] = df1.groupby(['Vehicle_id', 'Date_Pickup'])['Price'].transform('mean')
+  df1['Avg_type_car_prices'] = df1.groupby(['Vehicle_Name', 'Date_Pickup'])['Price'].transform('mean')
+  df1['Date_Pickup'] = pd.to_datetime(df1['Date_Pickup'])
 
   return df1
 
