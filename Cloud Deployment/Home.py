3b6375807d036002f2fed4e09dbbd97a623ce91f
Christian Hollar <hollarc@lafayette.edu>
2022-11-20 16:34:59 -0500

    Web Page Viz Added

--- Cloud Deployment/Home.py
+++ Cloud Deployment/Home.py
@@ -4,6 +4,9 @@ import pickle
 import numpy as np
 from datetime import datetime as dt
 
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
 def deploy():
 
     st.title('Luxury Vehicle Rental Car Forecasting')
@@ -23,10 +26,10 @@ def deploy():
         day_of_week = dt.strptime(date_input,'%Y-%m-%d').strftime('%A')
         score = get_score(date_input)
 
-        st.write(f'Selected Date: {date_input}')
-        st.write(f'Trend: {str(trend)}')
-        st.write(f'Day of the Week: {day_of_week}')
-        st.write(f'Score: {score}')
+        st.write(f'**Selected Date:** {date_input}')
+        st.write(f'**Trend:** {str(trend)} :chart_with_upwards_trend:')
+        st.write(f'**Day of the Week:** {day_of_week} :calendar:')
+        st.write(f'**Score:** {score}')
 
         loaded_model = pickle.load(open('Models/luxury_price_model.pkl', 'rb'))
         df_inputs = pd.read_csv('Data/Input_Price.csv')
@@ -35,7 +38,22 @@ def deploy():
         pred = loaded_model.predict(pred_input)
         ans = str(round(pred[0],2))
         
-        st.write(f'We predict the price will be ${ans}!')
+        st.write(f'We predict the price will be **${ans}** 	:money_with_wings:')
+        
+        df_model = pd.read_csv('Data/ModelData.csv')
+        year_month_formatter = mdates.DateFormatter("%Y-%m")
+        dates = pd.to_datetime(df_model['Date'])
+        plt.plot(dates,df_model['Average Price'])
+        single_date = [date_input]
+        single_date = pd.to_datetime(single_date)
+        single_value = [pred[0]]
+        
+        plt.plot(single_date,single_value,marker='x')
+        plt.gca().xaxis.set_major_formatter(year_month_formatter) 
+        plt.ylabel('Price ($)')
+        plt.xlabel('Date (YYYY-MM)')
+        plt.title('Luxury Vehicle Pricing')
+        st.pyplot(plt.gcf())
     else:
         st.write('Waiting for input...')
 
@@ -53,10 +71,18 @@ def deploy():
         mbenz_value = get_mercedes_benz(date_input)
         sfarm_value = get_state_farm(date_input)
 
-        st.write(f'Selected Date: {str(date_input)}')
-        st.write(f'Average Price: {str(price_value)}')
-        st.write(f'Mercedez-Benz: {str(mbenz_value)}')
-        st.write(f'State Farm: {str(sfarm_value)}')
+        st.write(f'**Selected Date:** {str(date_input)}')
+        st.write(f'**Average Price:** ${str(price_value)}')
+
+        if(mbenz_value == 1):
+            st.write('There is an event at Mercedes-Benz Stadium :tada:')
+        else:
+            st.write('There is NOT an event at Mercedes-Benz Stadium :x:')
+
+        if(sfarm_value == 1):
+            st.write('There is an event at State Farm Arena :tada:')
+        else:
+            st.write('There is NOT an event at State Farm Arena :x:')
 
         loaded_model = pickle.load(open('Models/count_model.pkl', 'rb'))
         pred_input = np.array([price_value, score_value, d, mbenz_value,sfarm_value])
@@ -68,13 +94,31 @@ def deploy():
         
         if(ans>0):
             ans = str(round(ans,2))+'%'
-            st.write(f'We predict the demand will be {ans} greater than average!')
+            st.write(f'We predict the demand will be **{ans}** greater than average!')
         else:
             ans = abs(ans)
             ans = str(round(ans,2))+'%'
-            st.write(f'We predict the demand will be {ans} less than average!')
+            st.write(f'We predict the demand will be **{ans}** less than average!')
+    
+        df_model = pd.read_csv('Data/ModelData.csv')
+        year_month_formatter = mdates.DateFormatter("%Y-%m")
+        dates = pd.to_datetime(df_model['Date'])
+        plt.plot(dates,df_model['Count'])
+        single_date = [date_input]
+        single_date = pd.to_datetime(single_date)
+        single_value = [pred[0]]
+        
+        plt.plot(single_date,single_value,marker='x')
+        plt.gca().xaxis.set_major_formatter(year_month_formatter) 
+        plt.ylabel('Number of Luxury Vehicles Actively Rented')
+        plt.xlabel('Date (YYYY-MM)')
+        plt.title('Luxury Vehicle Demand')
+        st.pyplot(plt.gcf())
+
     else:
         st.write('Waiting for input...')
+
+
 
 # Model Methods
 def get_score(date):
